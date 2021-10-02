c0259cd4b6e63da9b17a93c3fdc37e9d90a97b08
binyamin12302 <binyamin12302@gmail.com>
2021-10-02 13:16:58 +0300

    added debounce

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Axios from "axios";
-import { useEffect, useState, useCallback, useMemo } from "react";
+import { useState, useCallback, useMemo } from "react";
 import Movie from "./Movie";
 import Loading from "./Loading";
 import NotFound from "./NotFound";
@@ -10,7 +10,7 @@ import ReactPaginate from 'react-paginate';
 
 function App() {
   const [movies, setMovies] = useState({
-    page: JSON.parse(localStorage.getItem('allTasks')) || 1,
+    page: JSON.parse(localStorage.getItem('pageNumber')) || 1,
     results: [],
     total_pages: 500
   });
@@ -18,28 +18,23 @@ function App() {
   const [searchInput, setSearchInput] = useState("");
   const [filteredmovies, setFilteredmovies] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [isMovieFound, setIsMovieFound] = useState(false);
+  const [isMovieFoundAfterSearch, setIsMovieFoundAfterSearch] = useState(false);
 
-  useEffect(() => {
-    const ourRequest = Axios.CancelToken.source();
+  const getMovies = useMemo(
+    () =>
+      debounce(async function (selected) {
+        try {
+          const response = await Axios.get(`https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=fc974e5e89d3cfba7e0fee335ffc7bfa&page=${selected}`);
+          setMovies(response.data);
+          setIsLoading(false);
+        } catch (e) {
+          console.log("There was a problem ww.");
+        }
+      }, 750),
+    []
+  );
 
-    async function fetchData() {
-      try {
-        const response = await Axios.get(`https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=fc974e5e89d3cfba7e0fee335ffc7bfa&page=${movies.page}`, { cancelToken: ourRequest.token });
-        setMovies(response.data);
-        setIsLoading(false);
-      } catch (e) {
-        console.log("There was a problem ww.");
-      }
-    }
-    fetchData();
-
-    return () => {
-      ourRequest.cancel();
-    };
-  }, [movies.page]);
-
-  const handler = useMemo(
+  const searchMovie = useMemo(
     () =>
       debounce(async function (e) {
         try {
@@ -47,7 +42,7 @@ function App() {
           setFilteredmovies(response.data.results);
           setIsLoading(false);
           if (response.data.results.length === 0) {
-            setIsMovieFound(false);
+            setIsMovieFoundAfterSearch(false);
           }
         } catch (error) {
           console.log("There was a problem.");
@@ -56,60 +51,49 @@ function App() {
     []
   );
 
+
+  
+  const handlePageClick = useCallback(
+    data => {
+      let selected = data.selected
+
+      selected ? selected++ : selected = 1
+
+      setIsLoading(true);
+      getMovies(selected);
+      saveInLocalStorage(selected)
+    },
+    [getMovies]
+  );
+
+
   const handleInputChange = useCallback(
     e => {
       setSearchInput(e.target.value);
-      handler(e.target.value);
+      searchMovie(e.target.value);
       setIsLoading(true);
-      setIsMovieFound(true);
+      setIsMovieFoundAfterSearch(true);
     },
-    [handler]
+    [searchMovie]
   );
 
-  function handlePageClick(data) {
 
-    let selected = data.selected
-
-    if (selected) {
-      selected++
-    } else {
-      selected = 1
-    }
-
-    setIsLoading(true);
-    setMovies(prevstate => {
-      return { ...prevstate, page: selected };
-    });
-
-
-    saveInLocalStorage(selected)
+  function saveInLocalStorage(selected) {
+    localStorage.setItem("pageNumber", JSON.stringify(selected));
   }
 
 
-  function saveInLocalStorage(data) {
-    localStorage.setItem("allTasks", JSON.stringify(data));
-  }
+  const allMovies = movies.results.map((movie, index) => {
+    return <Movie isLoading={isLoading} movie={movie} key={index} />;
+  })
 
+  const allfilteredmovies = filteredmovies.map((movie, index) => {
+    return <Movie isLoading={isLoading} movie={movie} key={index} />;
+  })
 
-  const allMovies = isLoading ? (
-    <Loading />
-  ) : (
-    movies.results.map((movie, index) => {
-      return <Movie isLoading={isLoading} movie={movie} key={index} />;
-    })
-  );
+  const areFilteredMoviesFound = isMovieFoundAfterSearch ? allfilteredmovies : <NotFound />;
 
-  const allfilteredmovies = isLoading ? (
-    <Loading />
-  ) : (
-    filteredmovies.map((movie, index) => {
-      return <Movie isLoading={isLoading} movie={movie} key={index} />;
-    })
-  );
-
-  const isFoundfilteredmovies = isMovieFound ? allfilteredmovies : <NotFound />;
-
-  const content = searchInput === "" ? allMovies : isFoundfilteredmovies;
+  const content = searchInput === "" ? allMovies : areFilteredMoviesFound;
 
   return (
     <div>
@@ -139,7 +123,7 @@ function App() {
       />
 
       <main id="main">
-        {content}
+        {isLoading ? <Loading /> : content}
       </main>
 
     </div>
