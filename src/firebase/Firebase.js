a448afc5183d004c6234fc50829ac568aa790da3
binyamin12302 <binyamin12302@gmail.com>
2022-03-19 18:59:47 +0200

    firebase

--- src/firebase/Firebase.js
+++ src/firebase/Firebase.js
@@ -11,7 +11,7 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 
