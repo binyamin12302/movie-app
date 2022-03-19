079a47a2cc66e8d1b5c6d4d7c15d48875e811e2a
binyamin12302 <binyamin12302@gmail.com>
2022-03-19 14:44:41 +0200

    header changed

--- src/components/HomeGuest.js
+++ src/components/HomeGuest.js
@@ -62,7 +62,7 @@ function HomeGuest() {
 
   return (
     <main id="home-guest">
-      <h1>Hedaer me</h1>
+      <h1>Hedaer swwda</h1>
       <section>
         <h1 className="section-title">
           <i className="far fa-star"></i> Latest Movies
